a53f9e65d2cea3479273c4e3271c45643753d70c
Mauricio de Britto <mauricio@grupobritto.com.br>
2025-06-27 14:57:25 -0300

    Init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <main className="h-screen flex justify-center items-center bg-(--color-background-geral)">
+        <div>
+          <h1 className="text-(--color-texto)">Hello, World!</h1>
+        </div>
+      </main>
+    </>
+  );
+}
