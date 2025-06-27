8def17f6ca4457c124f5e2ba32bc17fac27750ab
Mauricio de Britto <mauricio@grupobritto.com.br>
2025-06-27 18:54:12 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <>
       <main className="flex h-screen items-center justify-center bg-(--color-background-geral)">
-        <div>
+        <div className="flex flex-col items-center justify-center space-y-2">
           <h1 className="text-(--color-texto)">Hello, VeriJus</h1>
+          <div className="w-32 rounded bg-neutral-100 p-2 text-center text-zinc-900 hover:bg-(--color-highlight) hover:text-white">
+            <Link href={'/dashboard'}>Dashboard</Link>
+          </div>
         </div>
       </main>
     </>
