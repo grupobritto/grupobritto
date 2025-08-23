c38ef994596da00f7552eee638936173b7191a60
Mauricio de Britto <mauricio@grupobritto.com.br>
2025-08-23 16:25:15 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <>
       <main className="flex h-screen items-center justify-center bg-(--color-background-geral)">
         <div>
-          <h1 className="text-(--color-texto)">Hello, World</h1>
+          <h1 className="text-(--color-texto)">Hello, World!</h1>
         </div>
       </main>
     </>
